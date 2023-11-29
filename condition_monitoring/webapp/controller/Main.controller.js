54c6cb556648b3e307dd9a4524306b98127dc2a2
Aron <hansick001@gmail.com>
2023-11-29 10:02:20 +0900

    재배환경 모니터링 레이아웃

--- condition_monitoring/webapp/controller/Main.controller.js
+++ condition_monitoring/webapp/controller/Main.controller.js
@@ -1,15 +1,25 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+	'sap/ui/model/json/JSONModel'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("conditionmonitoring.controller.Main", {
             onInit: function () {
+                var oDataModel = new JSONModel({})
+                var oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock/products.json"));
+                this.getView().setModel(oModel);
 
+                // document.getElementById('idDashboard').onclick();
+
+            },
+            onSliderMoved: function (oEvent) {
+                var fValue = oEvent.getParameter("value");
+                this.byId("containerLayout").setWidth(fValue + "%");
             }
         });
     });
