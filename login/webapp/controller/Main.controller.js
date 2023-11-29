4986d5de388b1bbf426e416c239daccb8e5bfe52
kalon <hansick001@gmail.com>
2023-11-30 00:47:27 +0900

    로그인 후 파라미터 담아서 메인 페이지 이동 처리

--- login/webapp/controller/Main.controller.js
+++ login/webapp/controller/Main.controller.js
@@ -1,16 +1,30 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    'sap/ui/model/json/JSONModel'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,CryptoJS) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("login.controller.Main", {
             onInit: function () {
+              var oModel = new JSONModel({logoPath:"/assets/image/logo.png"});
+              var oLoginUserModel = new JSONModel({CustID:""});
 
-            },
+              this.getView().setModel(oModel,"Logo");
+              this.getView().setModel(oLoginUserModel,"CustID");
+
+              var oRouter = this.getOwnerComponent().getRouter();
+              //Detail 라우터에 Pattern Matched 이벤트 붙이기
+              oRouter.getRoute('RouteMain').attachPatternMatched(this._patternMatched, this);
+          },
+          //라우터 패턴이 "일치할때마다" 실행
+          _patternMatched : function(oEvent) {
+            let oData = this.getView().getModel('Logo').getData().logoPath;
+            this.fnImageSet(oData);
+          },
             onLogin : function() {
                 var oDataModel = this.getView().getModel();
                 var oID = this.getView().byId("idLoginid");
@@ -26,19 +40,20 @@ sap.ui.define([
 
                 oDataModel.read(body, {
                     success : function(oReturn) {
-                        // oModel.setProperty("/list", oReturn.results);
-                        // 로그인 성공 메인 페이지 이동 로직 추가하기
-                        debugger;
-                        console.log("로그인 성공!!");
-                        console.log(oReturn);
-                        // 로그인 성공 메인 페이지 이동 로직 추가하기
-                        this.onMoveApp();
-                    }.bind(this),
-                    Error : function(oError) {
 
+                      let oData = this.getView().getModel('CustID').getData().CustID;
+
+                      // 로그인 성공 메인 페이지 이동 로직 추가하기
+                      if(oReturn.Loginid == '0000')
+                      {
                         sap.m.MessageToast.show("아이디나 비밀번호를 확인 후 다시 로그인해주세요.");
-
-                    }
+                      }
+                      else{
+                        this.getView().getModel('CustID').getData().CustID = oReturn.CustId
+                        this.onMoveApp();
+                      }
+                        
+                    }.bind(this)
                 });
             },
             onJoinus : function() {
@@ -64,7 +79,7 @@ sap.ui.define([
                         semanticObject: "Z03SE_HOME",
                         action: "display",
                       },
-                      params: { CustID: "test1111" },
+                      params: { CustID: this.getView().getModel('CustID').getData() },
                     })) ||
                   "";
         
