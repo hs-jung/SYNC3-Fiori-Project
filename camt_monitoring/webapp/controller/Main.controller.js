eb8225d278fb2183c04b5191d0d380187c2604c5
Aron <hansick001@gmail.com>
2023-11-29 10:02:42 +0900

    자재 재고 현황 대시보드 레이아웃

--- camt_monitoring/webapp/controller/Main.controller.js
+++ camt_monitoring/webapp/controller/Main.controller.js
@@ -1,15 +1,29 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+	'sap/ui/model/json/JSONModel'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,JSONModel) {
         "use strict";
 
         return Controller.extend("camtmonitoring.controller.Main", {
             onInit: function () {
+                 //Data 설정
+                 var oData = {  selectedBackground : "Dashboard" };         //json data
+                var oModel = new JSONModel(oData); 
+                //jsonModel을 View에서 사용하고 싶으면 => Data Binding.
+                this.getView().setModel(oModel, 'selectedBackground');
+                
+                var oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock/products.json"));
+                this.getView().setModel(oModel);
 
+                // document.getElementById('idDashboard').onclick();
+            },
+            onSliderMoved: function (oEvent) {
+                var fValue = oEvent.getParameter("value");
+                this.byId("containerLayout").setWidth(fValue + "%");
             }
         });
     });
